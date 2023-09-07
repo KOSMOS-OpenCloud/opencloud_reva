6988d242fcbe69fb24a23adbcd140444d215761a
André Duffeck <andre.duffeck@firondu.de>
2023-09-07 11:03:22 +0200

    More porting work and fixes

--- cmd/reva/ocm-share-get.go
+++ cmd/reva/ocm-share-get.go
@@ -24,68 +24,49 @@ import (
 	"io"
 
 	rpc "github.com/cs3org/go-cs3apis/cs3/rpc/v1beta1"
-	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
+	ocm "github.com/cs3org/go-cs3apis/cs3/sharing/ocm/v1beta1"
+	"github.com/cs3org/reva/v2/pkg/utils"
 )
 
-func unlockCommand() *command {
-	cmd := newCommand("unlock")
-	cmd.Description = func() string { return "remove a lock on a resource" }
-	cmd.Usage = func() string { return "Usage: unlock <resource_path>" }
-
-	idFlag := cmd.String("id", "", "lock id")
-
-	cmd.ResetFlags = func() {
-		*idFlag = ""
-	}
-
+func ocmShareGetCommand() *command {
+	cmd := newCommand("ocm-share-get")
+	cmd.Description = func() string { return "get the info of the OCM share" }
+	cmd.Usage = func() string { return "Usage: ocm-share-get" }
 	cmd.Action = func(w ...io.Writer) error {
 		if cmd.NArg() < 1 {
 			return errors.New("Invalid arguments: " + cmd.Usage())
 		}
 
-		fn := cmd.Args()[0]
+		ctx := getAuthContext()
 		client, err := getClient()
 		if err != nil {
 			return err
 		}
 
-		ctx := getAuthContext()
+		id := cmd.Arg(0)
 
-		ref := &provider.Reference{Path: fn}
-
-		// get lock from the id if set
-		var lock *provider.Lock
-		if *idFlag == "" {
-			getLockRes, err := client.GetLock(ctx, &provider.GetLockRequest{
-				Ref: ref,
-			})
-			if err != nil {
-				return err
-			}
-			if getLockRes.Status.Code != rpc.Code_CODE_OK {
-				return formatError(getLockRes.Status)
-			}
-			lock = getLockRes.Lock
-		} else {
-			lock = &provider.Lock{
-				LockId: *idFlag,
-			}
+		shareRes, err := client.GetOCMShare(ctx, &ocm.GetOCMShareRequest{
+			Ref: &ocm.ShareReference{
+				Spec: &ocm.ShareReference_Id{
+					Id: &ocm.ShareId{
+						OpaqueId: id,
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+		if shareRes.Status.Code != rpc.Code_CODE_OK {
+			return formatError(shareRes.Status)
 		}
 
-		res, err := client.Unlock(ctx, &provider.UnlockRequest{
-			Ref:  ref,
-			Lock: lock,
-		})
-
+		d, err := utils.MarshalProtoV1ToJSON(shareRes.Share)
 		if err != nil {
 			return err
 		}
 
-		if res.Status.Code != rpc.Code_CODE_OK {
-			return formatError(res.Status)
-		}
-
-		fmt.Println("OK")
+		fmt.Println(string(d))
 
 		return nil
 	}
