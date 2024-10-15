2403905/reva

Kept historical resource naming after renaming in activity for shares and public links.

internal/grpc/services/publicshareprovider/publicshareprovider.go
@@ -332,6 +332,7 @@ func (s *service) CreatePublicShare(ctx context.Context, req *link.CreatePublicS
 	default:
 		res.Status = status.NewOK(ctx)
 		res.Share = share
+		res.Opaque = utils.AppendPlainToOpaque(nil, "path", sRes.GetInfo().GetPath())
 	}
 
 	return res, nil
@@ -353,15 +354,15 @@ func (s *service) RemovePublicShare(ctx context.Context, req *link.RemovePublicS
 			Status: status.NewInternal(ctx, "error loading public share"),
 		}, err
 	}
-	if !publicshare.IsCreatedByUser(ps, user) {
-		sRes, err := gatewayClient.Stat(ctx, &provider.StatRequest{Ref: &provider.Reference{ResourceId: ps.ResourceId}})
-		if err != nil {
-			log.Err(err).Interface("resource_id", ps.ResourceId).Msg("failed to stat shared resource")
-			return &link.RemovePublicShareResponse{
-				Status: status.NewInternal(ctx, "failed to stat shared resource"),
-			}, err
-		}
 
+	sRes, err := gatewayClient.Stat(ctx, &provider.StatRequest{Ref: &provider.Reference{ResourceId: ps.ResourceId}})
+	if err != nil {
+		log.Err(err).Interface("resource_id", ps.ResourceId).Msg("failed to stat shared resource")
+		return &link.RemovePublicShareResponse{
+			Status: status.NewInternal(ctx, "failed to stat shared resource"),
+		}, err
+	}
+	if !publicshare.IsCreatedByUser(ps, user) {
 		if !sRes.GetInfo().GetPermissionSet().RemoveGrant {
 			return &link.RemovePublicShareResponse{
 				Status: status.NewPermissionDenied(ctx, nil, "no permission to delete public share"),
@@ -374,8 +375,10 @@ func (s *service) RemovePublicShare(ctx context.Context, req *link.RemovePublicS
 			Status: status.NewInternal(ctx, "error deleting public share"),
 		}, err
 	}
+	o := utils.AppendJSONToOpaque(nil, "resourceid", ps.GetResourceId())
+	o = utils.AppendPlainToOpaque(o, "path", sRes.GetInfo().GetPath())
 	return &link.RemovePublicShareResponse{
-		Opaque: utils.AppendJSONToOpaque(nil, "resourceid", ps.GetResourceId()),
+		Opaque: o,
 		Status: status.NewOK(ctx),
 	}, nil
 }
@@ -603,6 +606,7 @@ func (s *service) UpdatePublicShare(ctx context.Context, req *link.UpdatePublicS
 	res := &link.UpdatePublicShareResponse{
 		Status: status.NewOK(ctx),
 		Share:  updateR,
+		Opaque: utils.AppendPlainToOpaque(nil, "path", sRes.GetInfo().GetPath()),
 	}
 	return res, nil
 }

internal/grpc/services/usershareprovider/usershareprovider.go
@@ -248,6 +248,7 @@ func (s *service) CreateShare(ctx context.Context, req *collaboration.CreateShar
 	return &collaboration.CreateShareResponse{
 		Status: status.NewOK(ctx),
 		Share:  createdShare,
+		Opaque: utils.AppendPlainToOpaque(nil, "path", sRes.GetInfo().GetPath()),
 	}, nil
 }
 
@@ -296,6 +297,7 @@ func (s *service) RemoveShare(ctx context.Context, req *collaboration.RemoveShar
 	}
 
 	o := utils.AppendJSONToOpaque(nil, "resourceid", share.GetResourceId())
+	o = utils.AppendPlainToOpaque(o, "path", sRes.GetInfo().GetPath())
 	if user := share.GetGrantee().GetUserId(); user != nil {
 		o = utils.AppendJSONToOpaque(o, "granteeuserid", user)
 	} else {
@@ -447,6 +449,7 @@ func (s *service) UpdateShare(ctx context.Context, req *collaboration.UpdateShar
 	res := &collaboration.UpdateShareResponse{
 		Status: status.NewOK(ctx),
 		Share:  share,
+		Opaque: utils.AppendPlainToOpaque(nil, "path", sRes.GetInfo().GetPath()),
 	}
 	return res, nil
 }

KOSMOS-OpenCloud/opencloud_reva

internal/grpc/services/publicshareprovider/publicshareprovider.go
@@ -332,6 +332,7 @@ func (s *service) CreatePublicShare(ctx context.Context, req *link.CreatePublicS
 	default:
 		res.Status = status.NewOK(ctx)
 		res.Share = share
+		res.Opaque = utils.AppendPlainToOpaque(nil, "resourcename", sRes.GetInfo().GetName())
 	}
 
 	return res, nil
@@ -353,15 +354,15 @@ func (s *service) RemovePublicShare(ctx context.Context, req *link.RemovePublicS
 			Status: status.NewInternal(ctx, "error loading public share"),
 		}, err
 	}
-	if !publicshare.IsCreatedByUser(ps, user) {
-		sRes, err := gatewayClient.Stat(ctx, &provider.StatRequest{Ref: &provider.Reference{ResourceId: ps.ResourceId}})
-		if err != nil {
-			log.Err(err).Interface("resource_id", ps.ResourceId).Msg("failed to stat shared resource")
-			return &link.RemovePublicShareResponse{
-				Status: status.NewInternal(ctx, "failed to stat shared resource"),
-			}, err
-		}
 
+	sRes, err := gatewayClient.Stat(ctx, &provider.StatRequest{Ref: &provider.Reference{ResourceId: ps.ResourceId}})
+	if err != nil {
+		log.Err(err).Interface("resource_id", ps.ResourceId).Msg("failed to stat shared resource")
+		return &link.RemovePublicShareResponse{
+			Status: status.NewInternal(ctx, "failed to stat shared resource"),
+		}, err
+	}
+	if !publicshare.IsCreatedByUser(ps, user) {
 		if !sRes.GetInfo().GetPermissionSet().RemoveGrant {
 			return &link.RemovePublicShareResponse{
 				Status: status.NewPermissionDenied(ctx, nil, "no permission to delete public share"),
@@ -374,8 +375,10 @@ func (s *service) RemovePublicShare(ctx context.Context, req *link.RemovePublicS
 			Status: status.NewInternal(ctx, "error deleting public share"),
 		}, err
 	}
+	o := utils.AppendJSONToOpaque(nil, "resourceid", ps.GetResourceId())
+	o = utils.AppendPlainToOpaque(o, "resourcename", sRes.GetInfo().GetName())
 	return &link.RemovePublicShareResponse{
-		Opaque: utils.AppendJSONToOpaque(nil, "resourceid", ps.GetResourceId()),
+		Opaque: o,
 		Status: status.NewOK(ctx),
 	}, nil
 }
@@ -603,6 +606,7 @@ func (s *service) UpdatePublicShare(ctx context.Context, req *link.UpdatePublicS
 	res := &link.UpdatePublicShareResponse{
 		Status: status.NewOK(ctx),
 		Share:  updateR,
+		Opaque: utils.AppendPlainToOpaque(nil, "resourcename", sRes.GetInfo().GetName()),
 	}
 	return res, nil
 }

internal/grpc/services/usershareprovider/usershareprovider.go
@@ -248,6 +248,7 @@ func (s *service) CreateShare(ctx context.Context, req *collaboration.CreateShar
 	return &collaboration.CreateShareResponse{
 		Status: status.NewOK(ctx),
 		Share:  createdShare,
+		Opaque: utils.AppendPlainToOpaque(nil, "resourcename", sRes.GetInfo().GetName()),
 	}, nil
 }
 
@@ -296,6 +297,7 @@ func (s *service) RemoveShare(ctx context.Context, req *collaboration.RemoveShar
 	}
 
 	o := utils.AppendJSONToOpaque(nil, "resourceid", share.GetResourceId())
+	o = utils.AppendPlainToOpaque(o, "resourcename", sRes.GetInfo().GetName())
 	if user := share.GetGrantee().GetUserId(); user != nil {
 		o = utils.AppendJSONToOpaque(o, "granteeuserid", user)
 	} else {
@@ -447,6 +449,7 @@ func (s *service) UpdateShare(ctx context.Context, req *collaboration.UpdateShar
 	res := &collaboration.UpdateShareResponse{
 		Status: status.NewOK(ctx),
 		Share:  share,
+		Opaque: utils.AppendPlainToOpaque(nil, "resourcename", sRes.GetInfo().GetName()),
 	}
 	return res, nil
 }