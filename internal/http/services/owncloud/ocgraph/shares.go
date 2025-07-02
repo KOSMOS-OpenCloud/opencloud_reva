cs3org/reva

shares: fix parent reference

internal/http/services/owncloud/ocgraph/shares.go
@@ -219,7 +219,6 @@ func (s *svc) share(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewEncoder(w).Encode(&ListResponse{
 		Value: lgPerm,
 	})
-
 }
 
 func (s *svc) createLink(w http.ResponseWriter, r *http.Request) {
@@ -327,12 +326,11 @@ func (s *svc) createLink(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	_ = json.NewEncoder(w).Encode(lgPerm)
-
 }
 
 func encodeSpaceIDForShareJail(res *provider.ResourceInfo) string {
 	return spaces.EncodeResourceID(res.Id)
-	//return spaces.EncodeStorageSpaceID(res.Id.StorageId, res.Path)
+	// return spaces.EncodeStorageSpaceID(res.Id.StorageId, res.Path)
 }
 
 func (s *svc) cs3ReceivedShareToDriveItem(ctx context.Context, rsi *gateway.ReceivedShareResourceInfo) (*libregraph.DriveItem, error) {
@@ -555,12 +553,24 @@ func (s *svc) getSharedByMe(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *svc) cs3ShareToDriveItem(ctx context.Context, info *provider.ResourceInfo, shares []*share) (*libregraph.DriveItem, error) {
+	parentSpacePath := path.Dir(spaces.RelativePathToSpaceID(info))
 
-	parentRelativePath := path.Dir(spaces.RelativePathToSpaceID(info))
+	relativePath, err := spaces.RelativePathToSpaceRoot(info)
+	if err != nil {
+		return nil, err
+	}
+	parentRelativePath := path.Dir(relativePath)
+	if parentRelativePath == "." {
+		parentRelativePath = ""
+	}
 
 	permissions, err := s.cs3sharesToPermissions(ctx, shares)
 	if err != nil {
 		return nil, err
+	}
+
+	if info.ParentId.SpaceId == "" {
+		info.ParentId.SpaceId = spaces.PathToSpaceID(parentSpacePath)
 	}
 
 	d := &libregraph.DriveItem{
@@ -569,10 +579,10 @@ func (s *svc) cs3ShareToDriveItem(ctx context.Context, info *provider.ResourceIn
 		LastModifiedDateTime: libregraph.PtrTime(utils.TSToTime(info.Mtime)),
 		Name:                 libregraph.PtrString(info.Name),
 		ParentReference: &libregraph.ItemReference{
-			DriveId: libregraph.PtrString(spaces.EncodeStorageSpaceID(info.Id.StorageId, info.Id.SpaceId)),
+			DriveId: libregraph.PtrString(fmt.Sprintf("%s$%s", info.Id.StorageId, info.Id.SpaceId)),
 			// DriveType: libregraph.PtrString(info.Space.SpaceType),
 			Id:   libregraph.PtrString(spaces.EncodeResourceID(info.ParentId)),
-			Name: libregraph.PtrString(path.Base(parentRelativePath)),
+			Name: libregraph.PtrString(path.Base(parentSpacePath)),
 			Path: libregraph.PtrString(parentRelativePath),
 		},
 		Permissions: permissions,

pkg/spaces/utils.go
@@ -23,6 +23,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
@@ -154,8 +155,22 @@ func spacesLevel(path string) int {
 	}
 }
 
+// Removes the Space ID from the path of a resource info, preserving the leading slash.
 func RelativePathToSpaceID(info *provider.ResourceInfo) string {
 	return strings.TrimPrefix(info.Path, info.Id.SpaceId)
+}
+
+// Returns the path relative to the space root.
+func RelativePathToSpaceRoot(info *provider.ResourceInfo) (relativePath string, err error) {
+	if info.Id.SpaceId == "" {
+		return "", errors.New("resourceInfo must contain a space ID")
+	}
+	_, spacePath, ok := DecodeStorageSpaceID(fmt.Sprintf("%s$%s", info.Id.StorageId, info.Id.SpaceId))
+	if !ok {
+		return "", fmt.Errorf("failed to decode storage space ID: %s", fmt.Sprintf("%s$%s", info.Id.StorageId, info.Id.SpaceId))
+	}
+
+	return filepath.Rel(spacePath, info.Path)
 }
 
 func ResourceIdToString(id *provider.ResourceId) string {

KOSMOS-OpenCloud/opencloud_reva

internal/http/services/owncloud/ocgraph/shares.go
@@ -219,7 +219,6 @@ func (s *svc) share(w http.ResponseWriter, r *http.Request) {
 	_ = json.NewEncoder(w).Encode(&ListResponse{
 		Value: lgPerm,
 	})
-
 }
 
 func (s *svc) createLink(w http.ResponseWriter, r *http.Request) {
@@ -327,12 +326,11 @@ func (s *svc) createLink(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	_ = json.NewEncoder(w).Encode(lgPerm)
-
 }
 
 func encodeSpaceIDForShareJail(res *provider.ResourceInfo) string {
 	return spaces.EncodeResourceID(res.Id)
-	//return spaces.EncodeStorageSpaceID(res.Id.StorageId, res.Path)
+	// return spaces.EncodeStorageSpaceID(res.Id.StorageId, res.Path)
 }
 
 func (s *svc) cs3ReceivedShareToDriveItem(ctx context.Context, rsi *gateway.ReceivedShareResourceInfo) (*libregraph.DriveItem, error) {
@@ -555,12 +553,25 @@ func (s *svc) getSharedByMe(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *svc) cs3ShareToDriveItem(ctx context.Context, info *provider.ResourceInfo, shares []*share) (*libregraph.DriveItem, error) {
+	parentSpacePath := path.Dir(spaces.RelativePathToSpaceID(info))
 
-	parentRelativePath := path.Dir(spaces.RelativePathToSpaceID(info))
+	relativePath, err := spaces.PathRelativeToSpaceRoot(info)
+	if err != nil {
+		return nil, err
+	}
+
+	parentRelativePath := path.Dir(relativePath)
+	if parentRelativePath == "." {
+		parentRelativePath = ""
+	}
 
 	permissions, err := s.cs3sharesToPermissions(ctx, shares)
 	if err != nil {
 		return nil, err
+	}
+
+	if info.ParentId.SpaceId == "" {
+		info.ParentId.SpaceId = spaces.PathToSpaceID(parentSpacePath)
 	}
 
 	d := &libregraph.DriveItem{
@@ -569,10 +580,10 @@ func (s *svc) cs3ShareToDriveItem(ctx context.Context, info *provider.ResourceIn
 		LastModifiedDateTime: libregraph.PtrTime(utils.TSToTime(info.Mtime)),
 		Name:                 libregraph.PtrString(info.Name),
 		ParentReference: &libregraph.ItemReference{
-			DriveId: libregraph.PtrString(spaces.EncodeStorageSpaceID(info.Id.StorageId, info.Id.SpaceId)),
+			DriveId: libregraph.PtrString(fmt.Sprintf("%s$%s", info.ParentId.StorageId, info.ParentId.SpaceId)),
 			// DriveType: libregraph.PtrString(info.Space.SpaceType),
 			Id:   libregraph.PtrString(spaces.EncodeResourceID(info.ParentId)),
-			Name: libregraph.PtrString(path.Base(parentRelativePath)),
+			Name: libregraph.PtrString(path.Base(parentSpacePath)),
 			Path: libregraph.PtrString(parentRelativePath),
 		},
 		Permissions: permissions,

pkg/spaces/utils.go
@@ -23,6 +23,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	provider "github.com/cs3org/go-cs3apis/cs3/storage/provider/v1beta1"
@@ -154,8 +155,22 @@ func spacesLevel(path string) int {
 	}
 }
 
+// Removes the Space ID from the path of a resource info, preserving the leading slash.
 func RelativePathToSpaceID(info *provider.ResourceInfo) string {
 	return strings.TrimPrefix(info.Path, info.Id.SpaceId)
+}
+
+// Returns the path relative to the space root.
+func PathRelativeToSpaceRoot(info *provider.ResourceInfo) (relativePath string, err error) {
+	if info.Id.SpaceId == "" {
+		return "", errors.New("resourceInfo must contain a space ID")
+	}
+	_, spacePath, ok := DecodeStorageSpaceID(fmt.Sprintf("%s$%s", info.Id.StorageId, info.Id.SpaceId))
+	if !ok {
+		return "", fmt.Errorf("failed to decode storage space ID: %s", fmt.Sprintf("%s$%s", info.Id.StorageId, info.Id.SpaceId))
+	}
+
+	return filepath.Rel(spacePath, info.Path)
 }
 
 func ResourceIdToString(id *provider.ResourceId) string {