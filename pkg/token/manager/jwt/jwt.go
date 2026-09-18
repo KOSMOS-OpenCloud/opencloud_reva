ec9926c55ba7cf6448e121a05cef6104dfd93115
flash <flash7777>
2026-09-18 14:05:26 +0200

    debug: log exact JWT validation error in DismantleToken (temp)

--- pkg/token/manager/jwt/jwt.go
+++ pkg/token/manager/jwt/jwt.go
@@ -20,6 +20,8 @@ package jwt
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"time"
 
 	auth "github.com/cs3org/go-cs3apis/cs3/auth/provider/v1beta1"
@@ -121,6 +123,12 @@ func (m *manager) DismantleToken(ctx context.Context, tkn string) (*user.User, m
 	token, err := jwt.ParseWithClaims(tkn, &claims{}, keyfunc, jwt.WithLeeway(time.Duration(m.conf.tokenTimeLeeway)*time.Second))
 
 	if err != nil {
+		// Debug: log the exact JWT validation error, token algorithm and length
+		tknAlg := ""
+		if len(tkn) > 20 {
+			tknAlg = tkn[:20]
+		}
+		fmt.Fprintf(os.Stderr, "[dismantle-debug] err=%v tkn_len=%d tkn_prefix=%q secret_len=%d\n", err, len(tkn), tknAlg, len(m.conf.Secret))
 		return nil, nil, errors.Wrap(err, "error parsing token")
 	}
 
